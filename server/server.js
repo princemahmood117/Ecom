e29a2c91a7115fb21e9d60cf3425367442bbb281
iftekhar <princemahmood117@gmail.com>
2026-07-06 04:44:52 +0600

    front json

--- server/server.js
+++ server/server.js
@@ -49,7 +49,17 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+// app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+const allowedOrigins = process.env.CLIENT_URLS.split(',');
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static('uploads'));
